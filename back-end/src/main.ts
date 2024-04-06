5442886e67d0cc88a9415a4114733c6972f81065
Hank Metzger <hank.metzger@gmail.com>
2024-04-05 22:22:08 -0500

    adding swagger docs

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = 4000;
 const CROSS_ORIGIN = 'http://localhost:3000';
@@ -12,6 +13,14 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Natera Test-Task API')
+    .setDescription('A sample API from the Test-Task API to communicate with.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT, () => {
     console.log('NestJS', `Server started on port ${PORT}`);
